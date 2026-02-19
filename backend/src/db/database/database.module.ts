2b0224841bdf4bf4cf7b39e29854812ae472a46c
Younes <younes.imouga@gmail.com>
2026-02-19 00:43:03 +0000

    BP-5 finished implementing the database connection

---
+++ backend/src/db/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+    imports: [
+        MongooseModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                return {
+                    uri: configService.getOrThrow<string>("database.url")
+                };
+            }
+        })
+    ]
+})
+
+export class DatabaseModule {}
