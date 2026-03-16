367251bd2c20d53e3917c1fdf06b0146e54d2b79
Younes <younes.imouga@gmail.com>
2026-03-16 13:54:00 +0000

    BP-29 required the attempt service in simulation module

--- backend/src/simulations/simulations.module.ts
+++ backend/src/simulations/simulations.module.ts
@@ -5,13 +5,15 @@ import { SimulationsController } from './simulations.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Simulation, SimulationSchema } from './schemas/simulation.schema';
 import { Attempt, AttemptSchema } from '../attempts/schemas/attempt.schema';
+import { AttemptsModule } from '../attempts/attempts.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Simulation.name, schema: SimulationSchema },
       { name: Attempt.name, schema: AttemptSchema },
-    ])
+    ]),
+    AttemptsModule,
   ],
   providers: [SimulationsService],
   controllers: [SimulationsController],
